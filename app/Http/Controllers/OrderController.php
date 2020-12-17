c7a108bb86866f71d11fba74e7cf1363857861cb
Tiffany <tiffany@Tiffanys-MacBook-Pro.local>
2020-12-17 14:36:52 -0600

    remove unused code, add read, update and delete methods

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Order;
 use App\Models\User;
 use Illuminate\Support\Carbon;
 
@@ -14,17 +15,6 @@ class OrderController extends Controller
         $this->validateData($request);
 
         $data = $request->only(['name', 'email', 'address', 'phone', 'quantity', 'total', 'payment.creditCardNumber', 'payment.expirationDate']);
-
-        // get current month
-        $currentMonth = Carbon::now()->month();
-
-        // check if user exists
-        if ($findUser = User::where('email', $data['email'])->first()) {
-            // check if users orders for the current month is greater than 3
-            if ($findUser->order()->whereMonth('created_at', $currentMonth)->count() >= 3) {
-                return 'You have reached your max order limit. Visit us again next month';
-            }
-        }
 
         // create user
         $user = User::create([
@@ -51,43 +41,82 @@ class OrderController extends Controller
         ]);
 
         // return order id for users order
-        return ['success' => 'Your order has been placed!', 'id' => $user->order->id];
+        return ['message' => 'Your order has been placed!', 'id' => $user->order->id];
     }
 
     protected function validateData(Request $request)
     {
-        $key = [
-            'address.address1' => 'address1',
-            'address.address2' => 'address2',
-            'address.city' => 'city',
-            'address.state' => 'state',
-            'address.zip' => 'zip',
-            'payment.creditCardNumber' => 'creditCardNumber',
-            'payment.expirationDate' => 'expirationDate'
-        ];
-
-        $validated = $request->validate([
+        $request->validate([
             'name' => 'required|string',
-            'email' => 'required|string|unique',
-            $key['address.address1'] => 'required|string',
-            $key['address.address2'] => 'string',
-            $key['address.city'] => 'required|string',
-            $key['address.state'] => 'required|string',
-            $key['address.zip'] => 'required|string',
+            'email' => 'required|string|unique:users,email',
+            'address.address1' => 'required|string',
+            'address.address2' => 'nullable|string',
+            'address.city' => 'required|string',
+            'address.state' => 'required|string',
+            'address.zip' => 'required|string',
             'phone' => 'required|string',
             'quantity' => 'integer|max:3',
-            $key['payment.creditCardNumber'] => 'required|string',
-            $key['payment.expirationDate'] => 'required|string'
+            'payment.creditCardNumber' => 'required|string',
+            'payment.expirationDate' => 'required|string'
         ]);
+    }
 
-        \Log::info('validated', [
-            'validation' => $validated
-        ]);
-        // if ($validated) {
-        //     return true;
-        // }
+    public function get()
+    {
+        // get the first user
+        if (! $user = User::with('order', 'payment')->first()) {
+            return ['message' => 'resource not found'];
+        }
 
-        // return the errors
-        // return $validated->toArray();
+        $data = [
+            'name' => $user->name,
+            'email' => $user->email,
+            'address' => [
+                'street1' => $user->address1,
+                'street2' => $user->address2,
+                'city' => $user->city,
+                'state' => $user->state,
+                'zip' => $user->zip,
+            ],
+            'phone' => $user->phone,
+            'payment' => [
+                'ccNum' => $user->payment->credit_card_number,
+                'exp' => $user->payment->expiration_date
+            ],
+            'quantity' => $user->order->quantity,
+            'total' => $user->order->price,
+            'orderDate' => $user->order->created_at->toDateTimeString(),
+            'fulfilled' => $user->order->fulfilled
+        ];
+        
+        return $data;
+    }
+
+    public function update(Request $request)
+    {
+        $params = $request;
+
+        if (! $order = Order::find($params['id'])) {
+            return ['message' => 'resource not found'];
+        }
+
+        $order->update(['fulfilled' => (bool)$params['fulfilled']]);
+
+        return ['message' => 'resource updated successfully'];
+    }
+
+    public function delete($id)
+    {
+        // find the order
+        if (! $order = Order::find($id)) {
+            return ['message' => 'resource not found'];
+        }
+
+        $userId = $order->user->id;
+
+        $order->delete();
+        User::find($userId)->payment->delete();
+        
+        return ['message' => 'resource deleted successfully'];
     }
 }
